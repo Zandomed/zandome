07607c076f2e8a992ae12953d317bfef9ab14da3
Miguel Mendoza <miguelemrdev@gmail.com>
2020-12-23 12:56:28 -0400

    Layout custom by pathname router

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,23 +5,41 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import React, { ReactNodeArray, useEffect } from 'react';
+import React, {
+   Component,
+   Fragment,
+   ReactNodeArray,
+   useEffect,
+   useState,
+} from 'react';
 import PropTypes, { ReactElementLike } from 'prop-types';
 import { useStaticQuery, graphql, withPrefix } from 'gatsby';
+import {
+   globalHistory,
+   HistoryListenerParameter,
+   HistoryLocation,
+} from '@reach/router';
 
 import Header from './header';
 
 type DataProps = {
    children: string | number | boolean | {} | ReactElementLike | ReactNodeArray;
-   location?: any;
 };
 
 const Layout: React.FC<DataProps> = ({ children }) => {
+   const [showLayout, setShowLayout] = useState(false);
    // console.log(window.location);
 
    useEffect(() => {
-      console.log(window.location);
-   }, [children]);
+      _toggleShowLayout(globalHistory.location);
+      return globalHistory.listen((history: HistoryListenerParameter) =>
+         _toggleShowLayout(history.location),
+      );
+   });
+
+   const _toggleShowLayout = (location: HistoryLocation): void => {
+      setShowLayout(location.pathname !== '/');
+   };
 
    const data = useStaticQuery(graphql`
       query SiteTitleQuery {
@@ -33,27 +51,35 @@ const Layout: React.FC<DataProps> = ({ children }) => {
       }
    `);
 
-   return (
-      <>
-         <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-         <div
-            style={{
-               margin: `0 auto`,
-               maxWidth: 960,
-               padding: `0 1.0875rem 1.45rem`,
-            }}>
-            <main>{children}</main>
-            <footer
-               style={{
-                  marginTop: `2rem`,
-               }}>
-               © {new Date().getFullYear()}, Built with
-               {` `}
-               <a href="https://www.gatsbyjs.com">Gatsby</a>
-            </footer>
-         </div>
-      </>
-   );
+   const _getView = () => {
+      if (showLayout) {
+         return (
+            <Fragment>
+               <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+               <div
+                  style={{
+                     margin: `0 auto`,
+                     maxWidth: 960,
+                     padding: `0 1.0875rem 1.45rem`,
+                  }}>
+                  <main>{children}</main>
+                  <footer
+                     style={{
+                        marginTop: `2rem`,
+                     }}>
+                     © {new Date().getFullYear()}, Built with
+                     {` `}
+                     <a href="https://www.gatsbyjs.com">Gatsby</a>
+                  </footer>
+               </div>
+            </Fragment>
+         );
+      } else {
+         return <main>{children}</main>;
+      }
+   };
+
+   return _getView();
 };
 
 Layout.propTypes = {
--- src/wrap-with-provider.tsx
+++ src/wrap-with-provider.tsx
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 import Layout from './components/layout';
 import { GlobalStyles } from './styles/global-styles';
 
-const WrapWithProvider = ({ children }) => {
+const WrapWithProvider = ({ children , ...props}) => {
    return (
       <Fragment>
          <GlobalStyles />
